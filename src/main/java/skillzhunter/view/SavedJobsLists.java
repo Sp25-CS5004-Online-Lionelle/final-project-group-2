051d57a15860c62f81ed73cca8bb00027818759f
eleahburman <eleahjburman@gmail.com>
2025-04-08 17:29:40 -0400

    Get controller properly to get all saved jobs from model.

--- src/main/java/skillzhunter/view/SavedJobsLists.java
+++ src/main/java/skillzhunter/view/SavedJobsLists.java
@@ -4,12 +4,18 @@ package skillzhunter.view;
 import java.util.ArrayList;
 import java.util.List;
 import skillzhunter.model.JobRecord;
+import skillzhunter.controller.IController;
 
 public class SavedJobsLists {
   private static List<JobRecord> savedJobs = new ArrayList<>();
+  private static IController controller;
 
   //observer pattern to notify views when the list changes
   private static List<JobView> observers = new ArrayList<>();
+
+  public static void setController(IController newController) {
+    SavedJobsLists.controller = newController;
+  }
 
   public static void addObserver(JobView view) {
     if (!observers.contains(view)) {
@@ -22,15 +28,20 @@ public class SavedJobsLists {
   }
 
   public static void addSavedJob(JobRecord job) {
+    System.out.println("SAVEDJOBSLIST.JAVA - add saved job:" + job);
     if (!savedJobs.contains(job)) {
-      savedJobs.add(job);
+      controller.getAddJob(job);
+      savedJobs = controller.getSavedJobs();
       notifyObservers();
     }
   }
 
   public static void removeSavedJob(JobRecord job) {
     if (savedJobs.contains(job)) {
-      savedJobs.remove(job);
+      //savedJobs.remove(job);
+      controller.getRemoveJob(job.id());
+      savedJobs = controller.getSavedJobs();
+      System.out.println("\tRemoved job new list list");
       notifyObservers();
     }
   }
@@ -40,8 +51,10 @@ public class SavedJobsLists {
   }
 
   private static void notifyObservers() {
+
     for (JobView observer : observers) {
-      if (observer instanceof SavedJobTab) {
+      if (observer instanceof SavedJobsTab) {
+        System.out.println("SAVEDJOBSLIST.JAVA - setting view jobsList to:" + getSavedJobs());
         observer.setJobsList(getSavedJobs());
       }
     }
